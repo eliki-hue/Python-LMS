b9af9c5c61f1b859ecf829f92bca3f9b7a27af75
ELIJAH KIRAGU <eliki13720@gmail.com>
2024-08-12 12:22:21 +0300

    added robotic course

--- learning/models.py
+++ learning/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    LEVEL_CHOICES = [
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+        ('robotics', 'Robotics')
+    ]
+    title = models.CharField(max_length=100)
+    level = models.CharField(max_length=20, choices=LEVEL_CHOICES)
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
