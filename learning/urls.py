9751d88f766ba31d47b2bee29e5cd11cf22c7096
ELIJAH KIRAGU <eliki13720@gmail.com>
2024-08-12 12:43:52 +0300

    updated the laerning urls

--- learning/urls.py
+++ learning/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),
-    path('course/<int:pk>/', views.lesson_detail, name='lesson_detail'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('', views.course_list, name='course_list'),
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
+    path('course/<int:course_id>/lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
 ]
