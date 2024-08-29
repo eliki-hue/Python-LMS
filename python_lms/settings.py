0559a55323ec10fcaa0691c652aceea34a01214d
Elijah Kiragu <95278300+eliki-hue@users.noreply.github.com>
2024-08-29 11:36:54 +0300

    created runtime and procfile files

--- python_lms/settings.py
+++ python_lms/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,14 +96,28 @@ WSGI_APPLICATION = 'python_lms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if config('MODE')=='dev':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER':config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST':config('DB_HOST'),
+            'PORT':''
+            
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default= config('DATABASE_URL')
+        )
+    }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast= Csv())
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
